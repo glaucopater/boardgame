eb69d356a9163760d0a33e323d394b8385b09d52
Glauco <glauco.pater@gmail.com>
2022-01-22 22:09:00 +0100

    Add demo Update info

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import logo from "./logo.svg";
 import { Board } from "./components/Board";
 import { MAX_HEIGHT, MAX_WIDTH } from "./config";
 import "./App.css";
+import { description, url, version } from "../package.json";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Board Game Random Map Generator</h1>
+        <h1>{description}</h1>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <main>
         <Board width={MAX_WIDTH} height={MAX_HEIGHT} />
       </main>
-      <footer className="App-footer">Made by GP</footer>
+      <footer className="App-footer">
+        Ver. {version} -  {new Date().getFullYear()} - (<a href={url}> Source </a>)
+      </footer>
     </div>
   );
 }
